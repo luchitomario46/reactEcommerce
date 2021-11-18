845d91a5ebd6d34972589f334c8570f9ff29c244
luchitomario46 <luis.s.perez.rojas@gmail.com>
2021-11-17 21:02:08 -0300

    primer commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import './NavBar.css';
+
+
+const NavBar = () => {
+	return (
+		<nav className='container'>
+			<ul>
+				<li>
+					<a href='https://www.google.com'>Inicio</a>
+				</li>
+				<li>
+					<a href='https://www.google.com'>Productos</a>
+				</li>
+				<li>
+					<a href='https://www.google.com'>Carrito de Compras</a>
+				</li>
+				<li>
+					<a href='https://www.google.com'>Contacto</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
